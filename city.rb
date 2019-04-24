afbd13f5e840145b60bb9130dd3930800b6e608c
Stephanie Fischler <sjfischler@gmail.com>
2019-04-24 19:41:15 -0400

    Complete next nearest assignment with tests

---
+++ city.rb
@@ -0,0 +1,10 @@
+class City
+
+attr_accessor :name, :x_coord, :y_coord
+
+  def initialize(name, x_coord, y_coord)
+    @name = name
+    @x_coord = x_coord
+    @y_coord = y_coord
+  end
+end
--- tsp_greed.rb
+++ tsp_greed.rb
@@ -1,25 +1,55 @@
-# DEF nearest_possible_neighbor(current_city)
-#   SET neighbor_cities TO current_city.neighbors
-#   SET nearest_neighbor TO first element in neighbor_cities
-#
-#   FOR current_neighbor IN neighbor_cities
-#     IF current_neighbor.visited IS FALSE AND current_neighbor.distance < nearest_neighbor.distance
-#       ASSIGN nearest_neighbor TO current_neighbor
-#     END IF
-#   END FOR
-#   RETURN nearest_neighbor
-# END DEF
+require_relative 'city'
 
-def nearest_neighbor(current_city)
-	neighbor_cities = current_city.neighbors
-	nearest_neighbor = neighbor_cities[0]
+class CityMap
 
-	for current_neighbor in neighbor_cities #iterate through neighbor cities
-		if !current_neighbor.visited? && current_neighbor.distance < nearest_neighbor.distance
-			nearest_neighbor = current_neighbor
-    else
-      current_neighbor.visited = true
-		end
-	end
-	return nearest_neighbor
+attr_accessor :neighbors
+
+  def initialize
+    @neighbors = []
+    @trip_itinerary = []
+  end
+
+  def insert(city_node)
+    @neighbors << city_node
+  end
+
+  def nearest_neighbor(current_city)
+      while @neighbors.length > 0
+        @neighbors.delete(current_city)
+        @trip_itinerary << current_city
+        current_nearest = @neighbors[0]
+
+      @neighbors.each do |neighbor|
+        dist_to_current = distance(neighbor, current_city)
+        dist_to_nearest = distance(current_nearest, current_city)
+        if dist_to_current < dist_to_nearest
+          current_nearest = neighbor
+        end
+      end
+        current_city = current_nearest
+     end
+      for num in 0...@trip_itinerary.length-1 do
+          puts "Starting from #{@trip_itinerary[num].name}, next closest is #{@trip_itinerary[num+1].name}"
+    end
+     return @trip_itinerary
+   end
+
+  def distance(city_1, city_2)
+    Math.hypot(city_1.x_coord - city_2.x_coord, city_1.y_coord - city_2.y_coord)
+  end
 end
+
+# TESTING TESTING TESTING
+# kings_landing = City.new('Kings Landing', 350, 360)
+# winterfell = City.new('Winterfell', 360, 370)
+# qarth = City.new('Qarth', 24, 11)
+# volantis = City.new('Volantis', 370, 380)
+#
+# new_map = CityMap.new
+#
+# new_map.insert(kings_landing)
+# new_map.insert(winterfell)
+# new_map.insert(qarth)
+# new_map.insert(volantis)
+#
+# new_map.nearest_neighbor(kings_landing)
